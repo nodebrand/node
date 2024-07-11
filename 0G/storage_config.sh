1dc8b74384088e602cef0e4a50196ce64a80da58
D-S-KIM <ehdtn7196@nate.com>
2024-07-11 15:33:09 +0900

    Create storage_config.sh

---
+++ 0G/storage_config.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+Y='\033[1;33m'
+N='\033[0m'
+R='\033[1;31m'
+G='\033[1;32m'
+P='\033[1;35m'
+
+update_profile_variable() {
+  local VAR_NAME="$1"
+  local PROMPT="$2"
+  local DEFAULT_VALUE="$3"
+
+  echo -e "${R}${PROMPT}${N}"
+  echo -e "${G}Ex: ${Y}${DEFAULT_VALUE}${N}"
+  read -p ": " USER_INPUT
+  VAR_VALUE="${USER_INPUT}"
+
+  if grep -q "^export $VAR_NAME=" ~/.bash_profile; then
+    sed -i "s|^export $VAR_NAME=.*|export $VAR_NAME=\"$VAR_VALUE\"|" ~/.bash_profile
+  else
+    echo "export $VAR_NAME=\"$VAR_VALUE\"" >> ~/.bash_profile
+  fi
+
+  export $VAR_NAME="$VAR_VALUE"
+}
+
+update_profile_variable_with_default() {
+  local VAR_NAME="$1"
+  local PROMPT="$2"
+  local DEFAULT_VALUE="$3"
+
+  echo -e "${R}${PROMPT}${N}"
+  echo -e "${G}Ex: ${Y}${DEFAULT_VALUE}${N}"
+  echo -e "${G}To use the default value, just press enter:${N}"
+  read -p ": " USER_INPUT
+  VAR_VALUE="${USER_INPUT:-$DEFAULT_VALUE}"
+
+  if grep -q "^export $VAR_NAME=" ~/.bash_profile; then
+    sed -i "s|^export $VAR_NAME=.*|export $VAR_NAME=\"$VAR_VALUE\"|" ~/.bash_profile
+  else
+    echo "export $VAR_NAME=\"$VAR_VALUE\"" >> ~/.bash_profile
+  fi
+
+}
+
+update_toml_files() 
+{
+    update_profile_variable "BLOCKCHAIN_RPC_ENDPOINT" 'Enter the exact BLOCKCHAIN_RPC_ENDPOINT(JSON-RPC)' "https://json-rpc.com or http://123.456.789:8545"
+    update_profile_variable "PRIVATE_KEY" 'Enter the exact PRIVATE_KEY (DO NOT INCLUDE LEADING 0x)' "691U4BETG97843926TEFU1493RVH395NODEBRAND59371RYE139EI134EBA1B135"
+    
+    update_profile_variable_with_default "ZGS_HOME" 'Enter the ZGS_HOME' "$HOME/0g-storage-node"
+    update_profile_variable_with_default "LOG_CONTRACT_ADDRESS" 'Enter the LOG_CONTRACT_ADDRESS' "0x8873cc79c5b3b5666535C825205C9a128B1D75F1"
+    update_profile_variable_with_default "MINE_CONTRACT" 'Enter the MINE_CONTRACT' "0x85F6722319538A805ED5733c5F4882d96F1C7384"
+    update_profile_variable_with_default "ZGS_LOG_SYNC_BLOCK" 'Enter the ZGS_LOG_SYNC_BLOCK' "802"
+    
+    ENR_ADDRESS=$(wget -qO- https://eth0.me)
+    if grep -q '^export ENR_ADDRESS=' ~/.bash_profile; then
+      sed -i 's|^export ENR_ADDRESS=.*|export ENR_ADDRESS="'"$ENR_ADDRESS"'"|' ~/.bash_profile
+    else
+      echo "export ENR_ADDRESS=\"$ENR_ADDRESS\"" >> ~/.bash_profile
+    fi
+
+    source ~/.bash_profile
+
+    sed -i.bak \
+      -e "s|^\s*#\?\s*miner_key\s*=.*|miner_key = \"$PRIVATE_KEY\"|" \
+      -e "s|^\s*#\?\s*network_dir\s*=.*|network_dir = \"network\"|" \
+      -e "s|^\s*#\?\s*network_enr_address\s*=.*|network_enr_address = \"$ENR_ADDRESS\"|" \
+      -e "s|^\s*#\?\s*network_enr_tcp_port\s*=.*|network_enr_tcp_port = 1234|" \
+      -e "s|^\s*#\?\s*network_enr_udp_port\s*=.*|network_enr_udp_port = 1234|" \
+      -e "s|^\s*#\?\s*network_libp2p_port\s*=.*|network_libp2p_port = 1234|" \
+      -e "s|^\s*#\?\s*network_discovery_port\s*=.*|network_discovery_port = 1234|" \
+      -e "s|^\s*#\s*rpc_listen_address\s*=.*|rpc_listen_address = \"0.0.0.0:5678\"|" \
+      -e "s|^\s*#\s*rpc_listen_address_admin\s*=.*|rpc_listen_address_admin = \"0.0.0.0:5679\"|" \
+      -e "s|^\s*#\?\s*rpc_enabled\s*=.*|rpc_enabled = true|" \
+      -e "s|^\s*#\?\s*db_dir\s*=.*|db_dir = \"db\"|" \
+      -e "s|^\s*#\?\s*log_config_file\s*=.*|log_config_file = \"log_config\"|" \
+      -e "s|^\s*#\?\s*log_directory\s*=.*|log_directory = \"log\"|" \
+      -e "s|^\s*#\?\s*network_boot_nodes\s*=.*|network_boot_nodes = [\"/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps\",\"/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAkzRjxK2gorngB1Xq84qDrT4hSVznYDHj6BkbaE4SGx9oS\",\"/ip4/18.167.69.68/udp/1234/p2p/16Uiu2HAm2k6ua2mGgvZ8rTMV8GhpW71aVzkQWy7D37TTDuLCpgmX\"]|" \
+      -e "s|^\s*#\?\s*log_contract_address\s*=.*|log_contract_address = \"$LOG_CONTRACT_ADDRESS\"|" \
+      -e "s|^\s*#\?\s*mine_contract_address\s*=.*|mine_contract_address = \"$MINE_CONTRACT\"|" \
+      -e "s|^\s*#\?\s*log_sync_start_block_number\s*=.*|log_sync_start_block_number = "$ZGS_LOG_SYNC_BLOCK"|" \
+      -e "s|^\s*#\?\s*blockchain_rpc_endpoint\s*=.*|blockchain_rpc_endpoint = \"$BLOCKCHAIN_RPC_ENDPOINT\"|" \
+      -e "s|^\s*#\?\s*\[sync\].*|\[sync\]|" \
+      -e "s|^\s*#\?\s*auto_sync_enabled\s*=.*|auto_sync_enabled = true|" \
+      -e "s|^\s*#\?\s*find_peer_timeout\s*=.*|find_peer_timeout = \"10s\"|" \
+      $ZGS_HOME/run/config.toml
+
+    network_boot_nodes=$(grep -oP 'network_boot_nodes = \[.*?\]' "$ZGS_HOME/run/config.toml")
+    NETWORK_ENR_ADDRESS=$(grep -oP 'network_enr_address = "\K[^"]+' $ZGS_HOME/run/config.toml)
+    BLOCKCHAIN_RPC_ENDPOINT=$(grep -oP 'blockchain_rpc_endpoint = "\K[^"]+' $ZGS_HOME/run/config.toml)
+    LOG_CONTRACT_ADDRESS=$(grep -oP 'log_contract_address = "\K[^"]+' $ZGS_HOME/run/config.toml)
+    MINE_CONTRACT_ADDRESS=$(grep -oP 'mine_contract_address = "\K[^"]+' $ZGS_HOME/run/config.toml)
+    ZGS_LOG_SYNC_BLOCK=$(grep -oP 'log_sync_start_block_number = \K\d+' $ZGS_HOME/run/config.toml)
+    STORAGE_NODE_PATH=$ZGS_HOME
+
+    echo -e "
+    +========================================================================================+
+    |            ${Y}_  ______  ___  _______  ___  ___   _  _____${N}    ____      ${P}___  _____${N}        |
+    |           ${Y}/ |/ / __ \/ _ \/ __/ _ )/ _ \/ _ | / |/ / _ \ ${N} / __/___  ${P}/ _ \/ ___/${N}        |
+    |          ${Y}/    / /_/ / // / _// _  / , _/ __ |/    / // /${N}  > _/_ _/ ${P}/ // / (_ /${N}         |
+    |         ${Y}/_/|_/\____/____/___/____/_/|_/_/ |_/_/|_/____/${N}  |_____/   ${P}\___/\___/${N}          |
+    |                                                                                        |
+    |                                                                                        |
+    |                 Your Variable Setting Updated ${G}Successfully.${N}                            |
+    +========================================================================================+
+
+    +========================================Variable========================================+
+                      
+                      STORAGE_NODE_PATH: ${G}"$STORAGE_NODE_PATH"${N}
+                      NETWORK_ENR_ADDRESS: ${G}"$NETWORK_ENR_ADDRESS"${N}
+                      BLOCKCHAIN_RPC_ENDPOINT: ${G}"$BLOCKCHAIN_RPC_ENDPOINT"${N}
+                      LOG_CONTRACT_ADDRESS: ${G}"$LOG_CONTRACT_ADDRESS"${N}
+                      MINE_CONTRACT_ADDRESS: ${G}"$MINE_CONTRACT_ADDRESS"${N}
+                      ZGS_LOG_SYNC_BLOCK: ${G}"$ZGS_LOG_SYNC_BLOCK"${N}
+                      ${G}"$network_boot_nodes"${N}
+    +========================================================================================+
+    "
+}
+
+update_toml_files
