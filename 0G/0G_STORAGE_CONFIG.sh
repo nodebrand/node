5f26b7062b4a6b789d0a95474b239ef0c537d3fb
Nodebrand <ehdtn7196@gmail.com>
2024-07-23 10:25:01 +0900

    Update 0G_STORAGE_CONFIG.sh

--- 0G/0G_STORAGE_CONFIG.sh
+++ 0G/0G_STORAGE_CONFIG.sh
@@ -47,24 +47,16 @@ update_profile_variable_with_default() {
 update_toml_files() 
 {
     update_profile_variable "BLOCKCHAIN_RPC_ENDPOINT" 'Enter the exact BLOCKCHAIN_RPC_ENDPOINT(JSON-RPC)' "https://json-rpc.com or http://123.456.789:8545"
-    # update_profile_variable "PRIVATE_KEY" 'Enter the exact PRIVATE_KEY (DO NOT INCLUDE LEADING 0x)' "691U4BETG97843926TEFU1493RVH395NODEBRAND59371RYE139EI134EBA1B135"
-
-    # sed -i.bak \
-    #   -e "s|^\s*#\?\s*miner_key\s*=.*|miner_key = \"$PRIVATE_KEY\"|" \
-    #   $CONFIG_FILE
 
     update_profile_variable_with_default "ZGS_HOME" 'Enter the ZGS_HOME' "$HOME/0g-storage-node"
     update_profile_variable_with_default "LOG_CONTRACT_ADDRESS" 'Enter the LOG_CONTRACT_ADDRESS' "0x8873cc79c5b3b5666535C825205C9a128B1D75F1"
     update_profile_variable_with_default "MINE_CONTRACT_ADDRESS" 'Enter the MINE_CONTRACT_ADDRESS' "0x85F6722319538A805ED5733c5F4882d96F1C7384"
     update_profile_variable_with_default "ZGS_LOG_SYNC_BLOCK" 'Enter the ZGS_LOG_SYNC_BLOCK' "802"
     update_profile_variable_with_default "CONFIG_FILE" 'Enter the CONFIG_FILE' "$ZGS_HOME/run/config.toml"
+
+    source ~/.bash_profile
     
     ENR_ADDRESS=$(wget -qO- https://eth0.me)
-
-    NETWORK_BOOT_NODE1="/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps"
-    NETWORK_BOOT_NODE2="/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAkzRjxK2gorngB1Xq84qDrT4hSVznYDHj6BkbaE4SGx9oS"
-    NETWORK_BOOT_NODE3="/ip4/18.167.69.68/udp/1234/p2p/16Uiu2HAm2k6ua2mGgvZ8rTMV8GhpW71aVzkQWy7D37TTDuLCpgmX"
-    NETWORK_BOOT_NODES="[\"$NETWORK_BOOT_NODE1\",\"$NETWORK_BOOT_NODE2\",\"$NETWORK_BOOT_NODE3\"]"
 
     if grep -q '^export ENR_ADDRESS=' ~/.bash_profile; then
       sed -i 's|^export ENR_ADDRESS=.*|export ENR_ADDRESS="'"$ENR_ADDRESS"'"|' ~/.bash_profile
@@ -72,6 +64,13 @@ update_toml_files()
       echo "export ENR_ADDRESS=\"$ENR_ADDRESS\"" >> ~/.bash_profile
     fi
     
+    source ~/.bash_profile
+    
+    NETWORK_BOOT_NODE1="/ip4/54.219.26.22/udp/1234/p2p/16Uiu2HAmTVDGNhkHD98zDnJxQWu3i1FL1aFYeh9wiQTNu4pDCgps"
+    NETWORK_BOOT_NODE2="/ip4/52.52.127.117/udp/1234/p2p/16Uiu2HAkzRjxK2gorngB1Xq84qDrT4hSVznYDHj6BkbaE4SGx9oS"
+    NETWORK_BOOT_NODE3="/ip4/18.167.69.68/udp/1234/p2p/16Uiu2HAm2k6ua2mGgvZ8rTMV8GhpW71aVzkQWy7D37TTDuLCpgmX"
+    NETWORK_BOOT_NODES="[\"$NETWORK_BOOT_NODE1\",\"$NETWORK_BOOT_NODE2\",\"$NETWORK_BOOT_NODE3\"]"
+
     if grep -q '^export NETWORK_BOOT_NODES=' ~/.bash_profile; then
       sed -i 's|^export NETWORK_BOOT_NODES=.*|export NETWORK_BOOT_NODES='\"'"$NETWORK_BOOT_NODES"'\"'|' ~/.bash_profile
     else
